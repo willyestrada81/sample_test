588107e89657301b3d218a762e5131792fd21d24
williamestrada <willy.estrada81@gmail.com>
2019-10-25 16:37:51 -0400

    Test to check if youtube logo exists added

--- test.js
+++ test.js
@@ -5,8 +5,10 @@ fixture`Running Test`
     .page`https://google.com`;
 
 test('Google Sample Test', async t => {
+    const logoExists = Selector('.ytd-topbar-logo-renderer').exists;
     await t
         .typeText('.gLFyf', 'youtube')
         .click('.gNO89b')
-        .navigateTo(`https://www.youtube.com/`);
+        .navigateTo(`https://www.youtube.com/`)
+        .expect(logoExists).ok();
 });
