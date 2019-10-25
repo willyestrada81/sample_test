b2a7763c6f4ef715fb49cc1e3857b45f1a6a4383
williamestrada <willy.estrada81@gmail.com>
2019-10-25 16:52:16 -0400

    Added test that checks if youtube logo is visible

--- test.js
+++ test.js
@@ -5,10 +5,10 @@ fixture`Running Test`
     .page`https://google.com`;
 
 test('Google Sample Test', async t => {
-    const logoExists = Selector('.ytd-topbar-logo-renderer').exists;
+    const logoExists = Selector('.yt-icon-container');
     await t
         .typeText('.gLFyf', 'youtube')
         .click('.gNO89b')
         .navigateTo(`https://www.youtube.com/`)
-        .expect(logoExists).ok();
+        .expect(logoExists).visible;
 });
